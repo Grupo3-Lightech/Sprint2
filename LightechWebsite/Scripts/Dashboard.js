6df025aa106b0e701460bdf66498f60dfa80717c
J3remias-Barcelos <jeremiasbarceloa96@gmail.com>
2023-05-04 09:26:33 -0300

    Subindo pasta da API

--- LightechWebsite/Scripts/Dashboard.js
+++ LightechWebsite/Scripts/Dashboard.js
@@ -1,6 +1,6 @@
 var menuItem = document.querySelectorAll('.item-menu');
 var expandirList = document.querySelector('#expandir-list');
-var menuSite = document.querySelector('.menu-lateral');
+var menuSite = document.querySelector('.menu-lateral'); 
 
 function selectLink() {
     menuItem.forEach((item) => 
